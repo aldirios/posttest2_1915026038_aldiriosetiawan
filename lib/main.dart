514b22997dbc408410b7497f4cd4e756aa6fa867
aldirios <84436853+aldirios@users.noreply.github.com>
2022-04-13 12:47:44 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:posttest2_1915026038_aldiriosetiawan/mainpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
